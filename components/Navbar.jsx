1734c13e42097a1332728791cd1394c2e9af39e9
Kunal <73430123+Kunal-ryfl@users.noreply.github.com>
2023-01-03 22:05:04 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
 import {AiOutlineShopping} from 'react-icons/ai'
-import Cart from './Cart'
-import {motion} from 'framer-motion'
 import { useStateContext } from '../context/StateContext'
 
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities } = useStateContext();
+  const { totalQuantities } = useStateContext();
   return (
     <>
     <div className="navbar-container">
    <Link href="/"> 
-
-<motion.div>  
+     <div>  
     <p className='logo-text'>SIGMA</p>  
-      </motion.div>
-   
-    
-
+      </div>
       </Link>
    
 
