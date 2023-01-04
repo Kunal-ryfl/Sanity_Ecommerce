944f2bc37bd941857f9615ed11e61ac0d1b9d09a
Kunal <73430123+Kunal-ryfl@users.noreply.github.com>
2023-01-04 11:37:32 +0530

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,5 +1,4 @@
 export {default as Footer} from './Footer'
-export {default as Cart} from './Cart'
 export {default as FooterBanner} from './FooterBanner'
 export {default as Navbar} from './Navbar'
 export {default as Layout} from './Layout'
