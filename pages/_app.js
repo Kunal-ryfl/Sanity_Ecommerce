ff280f15aa09cf8cfc7eaeefa975e99ff449efa2
Kunal-ryfl <kunalsharma070806@gmail.com>
2024-02-19 12:05:49 +0530

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,46 @@ import "../styles/globals.css";
 import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
-import {AnimatePresence} from 'framer-motion'
-
+import { AnimatePresence } from "framer-motion";
+import React from "react";
+import Router from "next/router";
+import { PropagateLoader } from "react-spinners";
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = React.useState(false);
+  React.useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("finished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+
   return (
-    <AnimatePresence exitBeforeEnter>  
+    <AnimatePresence exitBeforeEnter>
       <StateContext>
-      <Layout>
-        <Toaster />
-        <Component {...pageProps} />
-      </Layout>
-    </StateContext>
-     </AnimatePresence>
-    
+        <Layout>
+          <Toaster />
+          {loading ? (
+            <div style={{ minHeight: "100vh",width:'100vw',display:'flex',alignItems:'center' , justifyContent:'center'}}>
+              <PropagateLoader color='grey' />
+            </div>
+          ) : (
+            <Component {...pageProps} />
+          )}
+        </Layout>
+      </StateContext>
+    </AnimatePresence>
   );
 }
 
